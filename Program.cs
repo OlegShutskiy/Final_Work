79a216a9ff98495cad755810a2dccd51768806a0
Oleg <shuka.21@mail.ru>
2023-04-01 11:45:56 +0300

    Program

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿string [] startingArray = new string[] {"432", "147", "-m2", "computer science"};
+string[] resultArray = new string [0];
+
+foreach (var item in startingArray)
+{
+    if (item.Length <= 3)
+    {
+    Array.Resize(ref resultArray, resultArray.Length + 1);
+    resultArray [resultArray.Length-1] = item;
+    }
+}
+
+Console.WriteLine($"[{String.Join("; ", resultArray)}]");
